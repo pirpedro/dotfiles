0998bbc9fc1460c5a26fa494fc0db775339d4e55
Pedro Rodrigues <pir.pedro@gmail.com>
2022-03-15 21:59:34 -0300

    ZSH P10k and theme configuration for gnome.

--- home/.chezmoitemplates/packages/etcher.sh
+++ home/.chezmoitemplates/packages/etcher.sh
@@ -1,7 +1,7 @@
 {{ if lookPath "apt" }}
 curl -1sLf \
   'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' |
-  {{ .cmd.sudo }} -E distro=some-distro codename=some-codename arch=some-arch bash
+  {{ .cmd.sudo }} -E distro={{.chezmoi.osRelease.id}} version={{.chezmoi.osRelease.versionID}} codename={{.chezmoi.osRelease.versionCodename}} arch={{.chezmoi.arch}} bash
 {{ .cmd.sudo }} apt install -y balena-etcher-electron
 {{ else }}
 echo "No installation descriptor for this distro."
---
+++ home/.chezmoitemplates/packages/ulauncher.sh
@@ -0,0 +1,9 @@
+{{ if lookPath "apt" }}
+{{ .cmd.sudo }} add-apt-repository ppa:agornostal/ulauncher -y
+{{ .cmd.sudo }} apt update -y
+{{ .cmd.sudo }} apt -qq -y install ulauncher python3-pip
+pip3 install bs4 simpleeval pint faker validator-collection parsedatetime --user
+pip3 install --user "algoliasearch>=2.0,<3.0"
+{{ else }}
+echo "No installation descriptor for this distro."
+{{ end -}}
